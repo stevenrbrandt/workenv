6304bd1a03fb503f64ea7bb39580d2cfd460d455
Steven R. Brandt <sbrandt@cct.lsu.edu>
2021-03-30 10:42:06 -0500

    Changes to mkgcc.sh

--- bin/mkgcc.sh
+++ bin/mkgcc.sh
@@ -4,9 +4,9 @@
 #PBS -l nodes=1:ppn=20
 #PBS -A loni_cactus19
 
-module unload all
-module load gcc/6.4.0
-module load cuda/10.0
+# module unload all
+# module load gcc/6.4.0
+# module load cuda/10.0
 
 export GMP_VER=6.1.2
 export LZLIB_VER=1.21
@@ -14,9 +14,10 @@ export MPFR_VER=4.0.2
 export MPC_VER=1.1.0
 export GCC_VER=6.5.0
 export PARALLEL=4
+export GCC_BUILD_DIR=$HOME/build
+export GCC_INSTALL_DIR=$HOME/install
 
 set -x
-export GCC_BUILD_DIR=/project/sbrandt/chemora
 if [ ! -d $GCC_BUILD_DIR ]
 then
   echo "Directory $GCC_BUILD_DIR does not exist."
@@ -35,10 +36,10 @@ fi
 
 cd $GCC_BUILD_DIR
 cd lzip-${LZLIB_VER}
-export LZIP=$GCC_BUILD_DIR/install/lzip-${LZLIB_VER}/bin/lzip
+export LZIP=$GCC_INSTALL_DIR/lzip-${LZLIB_VER}/bin/lzip
 if [ ! -x $LZIP ]
 then
-  ./configure --prefix=$GCC_BUILD_DIR/install/lzip-${LZLIB_VER}
+  ./configure --prefix=$GCC_INSTALL_DIR/lzip-${LZLIB_VER}
   make -j ${PARALLEL} install
 fi
 
@@ -54,7 +55,7 @@ then
 fi
 
 cd $GCC_BUILD_DIR
-GMP_DIR=${GCC_BUILD_DIR}/install/gmp-${GMP_VER}
+GMP_DIR=${GCC_INSTALL_DIR}/gmp-${GMP_VER}
 if [ ! -r ${GMP_DIR}/lib/libgmp.so ]
 then
   cd gmp-${GMP_VER}
@@ -72,7 +73,7 @@ then
   tar xzf mpfr-${MPFR_VER}.tar.gz
 fi
 
-export MPFR_DIR=${GCC_BUILD_DIR}/install/mpfr-${MPFR_VER}
+export MPFR_DIR=${GCC_INSTALL_DIR}/mpfr-${MPFR_VER}
 
 cd $GCC_BUILD_DIR
 if [ ! -r $MPFR_DIR/lib/libmpfr.so ]
@@ -93,7 +94,7 @@ then
 fi
 
 cd $GCC_BUILD_DIR
-export MPC_DIR=${GCC_BUILD_DIR}/install/mpc-${MPC_VER}
+export MPC_DIR=${GCC_INSTALL_DIR}/mpc-${MPC_VER}
 if [ ! -r ${MPC_DIR}/lib/libmpc.so ]
 then
   cd mpc-${MPC_VER}
@@ -111,27 +112,28 @@ then
   tar xzvf gcc-${GCC_VER}.tar.gz
 fi
 
-cd $GCC_BUILD_DIR
-if [ ! -d nvptx-tools ]
-then
-  git clone https://github.com/MentorEmbedded/nvptx-tools.git
-else
-  cd nvptx-tools
-  git pull
-fi
+## NVPTX tools
+## cd $GCC_BUILD_DIR
+## if [ ! -d nvptx-tools ]
+## then
+##   git clone https://github.com/MentorEmbedded/nvptx-tools.git
+## else
+##   cd nvptx-tools
+##   git pull
+## fi
 
-export NVPTX_DIR=${GCC_BUILD_DIR}/install/nvptx
-if [ ! -r $NVPTX_DIR/bin/nvptx-none-as ]
-then
-  cd $GCC_BUILD_DIR/nvptx-tools
-  ./configure --prefix=${NVPTX_DIR}
-  make -j ${PARALLEL}
-  make install
-fi
+## export NVPTX_DIR=${GCC_INSTALL_DIR}/nvptx
+## if [ ! -r $NVPTX_DIR/bin/nvptx-none-as ]
+## then
+##   cd $GCC_BUILD_DIR/nvptx-tools
+##   ./configure --prefix=${NVPTX_DIR}
+##   make -j ${PARALLEL}
+##   make install
+## fi
 
 # See https://gcc.gnu.org/wiki/Offloading#A1._Building_accel_compiler
 cd $GCC_BUILD_DIR
-export GCC_DIR=${GCC_BUILD_DIR}/install/gcc-${GCC_VER}
+export GCC_DIR=${GCC_INSTALL_DIR}/gcc-${GCC_VER}
 export LD_LIBRARY_PATH=${GMP_DIR}/lib:${MPFR_DIR}/lib:${MPC_DIR}/lib:${GCC_DIR}/lib
 if [ ! -x ${GCC_DIR}/bin/gcc ]
 then
