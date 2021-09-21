16a086ced10d9ac1cf27090e54c5cef8fa8b8938
Steven R. Brandt <sbrandt@cct.lsu.edu>
2021-09-21 09:27:54 -0500

    Update build-amrex script

--- bin/build-amrex.sh
+++ bin/build-amrex.sh
@@ -1,27 +1,52 @@
 (
-set -x
+set -xe
 export MPIEXEC_EXECUTABLE=$(which mpiexec)
-BUILDID=2
+BUILDID=1
+if [ $BUILDID = 1 ]
+then
+    CUDA=OFF
+else
+    CUDA=ON
+fi
+echo "CUDA is $CUDA"
+sleep 5
+spack load gcc@9.4.0 cuda mpich%gcc@9.4.0 cmake%gcc@9.4.0
+rm -fr ~/repos/amrex/build${BUILDID}
 mkdir -p ~/repos/amrex/build${BUILDID}
 cd ~/repos/amrex/build${BUILDID}
 mkdir -p ~/install
-cmake -DCMAKE_BUILD_TYPE=Debug \
-  -DAMReX_PARTICLES=OFF \
-  -DAMReX_ASSERTIONS=ON \
-  -DAMReX_FORTRAN=OFF \
-  -DAMReX_CUDA=ON \
-  -DAMReX_GPU_BACKEND=CUDA \
-  -DAMReX_PIC=ON \
-  -DAMReX_OMP=ON \
-  -DAMReX_MPI=ON \
-  -DAMReX_TUTORIALS=ON \
-  -DCMAKE_C_COMPILER=$(which mpicc) \
-  -DCMAKE_CXX_COMPILER=$(which mpicxx) \
-  -DPYTHON_EXECUTABLE=$(which python3) \
-  -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXECUTABLE \
-  -DCMAKE_CUDA_COMPILER=$(which nvcc) \
-  -DCMAKE_INSTALL_PREFIX=$HOME/install/amrex${BUILDID} \
-  -DAMREX_GPUS_PER_NODE=1 \
-  ..
+if [ $CUDA = ON ]
+then
+  cmake -DCMAKE_BUILD_TYPE=Release \
+    -DAMReX_PARTICLES=ON \
+    -DAMReX_ASSERTIONS=Off \
+    -DAMReX_FORTRAN=ON \
+    -DAMReX_CUDA=$CUDA \
+    -DAMReX_GPU_BACKEND=CUDA \
+    -DAMReX_PIC=ON \
+    -DAMReX_OMP=ON \
+    -DAMReX_MPI=ON \
+    -DCMAKE_C_COMPILER=$(which mpicc) \
+    -DCMAKE_CXX_COMPILER=$(which mpicxx) \
+    -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXECUTABLE \
+    -DCMAKE_CUDA_COMPILER=$(which nvcc) \
+    -DCMAKE_INSTALL_PREFIX=$HOME/install/amrex${BUILDID} \
+    -DAMREX_GPUS_PER_NODE=1 \
+    .. |& tee ~/amrex-config${BUILDID}-log.txt
+else
+  cmake -DCMAKE_BUILD_TYPE=Release \
+    -DAMReX_PARTICLES=ON \
+    -DAMReX_ASSERTIONS=Off \
+    -DAMReX_FORTRAN=ON \
+    -DAMReX_CUDA=$CUDA \
+    -DAMReX_PIC=ON \
+    -DAMReX_OMP=ON \
+    -DAMReX_MPI=ON \
+    -DCMAKE_C_COMPILER=$(which mpicc) \
+    -DCMAKE_CXX_COMPILER=$(which mpicxx) \
+    -DMPIEXEC_EXECUTABLE=$MPIEXEC_EXECUTABLE \
+    -DCMAKE_INSTALL_PREFIX=$HOME/install/amrex${BUILDID} \
+    .. |& tee ~/amrex-config${BUILDID}-log.txt
+fi
 make -j6 install
-) 2>&1 | tee ~/amrex-build${BUILDID}-log.txt
+) |& tee ~/amrex-build${BUILDID}-log.txt
