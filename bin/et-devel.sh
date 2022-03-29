df2fcbbc8027390b1afd64042f6e9c9f2a6b52e3
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-03-29 03:54:36 -0500

    Add et-devel.sh

---
+++ bin/et-devel.sh
@@ -0,0 +1,3 @@
+curl -kLO https://raw.githubusercontent.com/gridaphobe/CRL/master/GetComponents
+chmod a+x GetComponents
+./GetComponents --parallel https://bitbucket.org/einsteintoolkit/manifest/raw/master/einsteintoolkit.th
