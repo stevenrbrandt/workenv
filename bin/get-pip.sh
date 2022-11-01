6b3942a8fc9e8e081cd6bf4061e8bff296d0813f
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-11-01 10:26:22 -0500

    Upgrade get-pip

---
+++ bin/get-pip.sh
@@ -0,0 +1,4 @@
+curl https://bootstrap.pypa.io/get-pip.py -o ~/get-pip.py
+python3 ~/get-pip.py
+rm ~/get-pip.py
+python3 -m pip --version
--- bin/get_pip.sh
+++
@@ -1,4 +0,0 @@
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py
-rm get-pip.py
-python3 -m pip --version
