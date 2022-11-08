5edce2cb0239c36037d5eb91f8ee468bb73825d4
Steven R. Brandt <sbrandt@cct.lsu.edu>
2022-11-08 11:55:01 -0600

    Add comment.

--- bin/doc-dev.py
+++ bin/doc-dev.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
+# USAGE: doc-dev.py docker-image-name
+#
+# This command starts a new shell in the current directory.
+# This shell has read/write access to the local directory,
+# but all other files and directories will come from the image.
+#
+# Note that inside the environment created by this new shell,
+# the current directory will be /usr/mnt, regardless of what
+# it appears to be to the external system.
+#
 import os
 import sys
 from subprocess import call
