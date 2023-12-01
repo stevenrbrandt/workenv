1262e81b0be42fc94720c6c2a0307386e94ad547
Steven R. Brandt <sbrandt@cct.lsu.edu>
2023-12-01 11:26:16 -0600

    Updates to cipher and rdme

--- bin/cipher.py
+++ bin/cipher.py
@@ -7,18 +7,29 @@ na = ord('a')
 nA = ord('A')
 n = nZ - nA + 1
 
+def cipher(c):
+    n1 = ord(c)
+    if na <= n1 and n1 <= nz:
+        n2 = na + (n1 - na + off) % n
+    elif nA <= n1 and n1 <= nZ:
+        n2 = nA + (n1 - nA + off) % n
+    else:
+        n2 = n1
+    return chr(n2)
+
 off = int(sys.argv[1])
 newargv = []
 for txt in sys.argv[2:]:
     newtxt = ""
     for c in txt:
-        n1 = ord(c)
-        if na <= n1 and n1 <= nz:
-            n2 = na + (n1 - na + off) % n
-        elif nA <= n1 and n1 <= nZ:
-            n2 = nA + (n1 - nA + off) % n
-        else:
-            n2 = n1
-        newtxt += chr(n2)
+        newtxt += cipher(c)
     newargv += [newtxt]
-print(" ".join(newargv))
+
+if len(sys.argv) > 2:
+    print(" ".join(newargv))
+elif len(sys.argv) == 2:
+    for line in sys.stdin.readlines():
+        newline = ""
+        for c in line:
+            newline += cipher(c)
+        sys.stdout.write(newline)
