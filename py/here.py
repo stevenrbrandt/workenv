06cd1188e89396ce925bd7773d866e2ded0c9274
Steven R. Brandt <sbrandt@cct.lsu.edu>
2024-01-15 18:25:33 -0600

    Add typing to here.py

--- py/here.py
+++ py/here.py
@@ -1,21 +1,25 @@
+from typing import Any
 from colored import colored
 import os
 import re
 
 _here = re.sub(r'/*$','/',os.path.realpath(os.getcwd()))
 
-def here(*args, **kwargs):
+def here(*args:Any, **kwargs:Any)->None:
     herell(False, *args, **kwargs)
 
-def herecc(*args, **kwargs):
+def herecc(*args:Any, **kwargs:Any)->None:
     herell(True,*args, **kwargs)
 
-def herell(usecc, *args, **kwargs):
+def herell(usecc:bool, *args:Any, **kwargs:Any)->None:
     import inspect
     stack = inspect.stack()
     frame = stack[2]
     if usecc:
-        herestr = re.sub(r"^herecc\((.*)\)$",r"HERE: \1:",frame.code_context[0].strip())
+        context = frame.code_context
+        assert context is not None
+        assert len(context) > 0
+        herestr = re.sub(r"^herecc\((.*)\)$",r"HERE: \1:",context[0].strip())
     else:
         herestr = "HERE:"
     fname = os.path.realpath(frame.filename)
