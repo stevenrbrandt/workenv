e9990a0b91d9551f083a86072a4195bff1e70706
Steven R. Brandt <sbrandt@cct.lsu.edu>
2026-07-06 12:44:08 -0500

    Change long-cmd behavior

--- bin/long-cmd-notify.sh
+++ bin/long-cmd-notify.sh
@@ -1,37 +1,32 @@
 TIMER_THING_HOST=$(hostname -s)
-TIMER_THING_LONG_TIME=60
 TIMER_THING_CMD=""
-TIMER_THING_EXIT_CODE=""
-TIMER_THING_TIME=""
-TIMER_THING_START=$SECONDS
 TIMER_THING_READY=0
-TIMER_THING_NO_MESSAGE=" vi vim ed nano ssh tmux screen vimdiff top git vix rdstory "
+
+__timer_thing_set_title__() {
+    printf '\033]0;%s\007' "$1"
+}
+
 __timer_thing_start__() {
     if [ $TIMER_THING_READY = 0 ]
     then
         if [ "${BASH_COMMAND}" != "__timer_thing_end__" ]
         then
             TIMER_THING_CMD="${BASH_COMMAND}"
-            TIMER_THING_START=$SECONDS
             TIMER_THING_READY=1
+            __timer_thing_set_title__ "⏳ $TIMER_THING_CMD"
         fi
     fi
 }
+
 __timer_thing_end__() {
+    TIMER_THING_EXIT_CODE=$?
     if [ $TIMER_THING_READY = 1 ]
     then
         TIMER_THING_READY=0
-        TIMER_THING_EXIT_CODE=$?
-        TIMER_THING_TIME=$(($SECONDS - $TIMER_THING_START))
-        TIMER_THING_MESSAGE="[$TIMER_THING_HOST:$PWD] $TIMER_THING_CMD => $TIMER_THING_EXIT_CODE (${TIMER_THING_TIME}s)" 
-        if [ $TIMER_THING_TIME -gt $TIMER_THING_LONG_TIME ]
-        then
-            TIMER_THING_CMD_1=${TIMER_THING_CMD%% *}
-            if ! echo $TIMER_THING_NO_MESSAGE | grep -qw "$TIMER_THING_CMD_1"; then
-                telegram-send "$TIMER_THING_MESSAGE"
-            fi
-        fi
     fi
+    __timer_thing_set_title__ "✅ $TIMER_THING_HOST:$PWD"
+    return $TIMER_THING_EXIT_CODE
 }
+
 trap __timer_thing_start__ DEBUG
 export PROMPT_COMMAND="__timer_thing_end__"
