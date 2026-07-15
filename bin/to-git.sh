524f9b10f230ade4612bbc71458c9117499e3ab5
Steven R. Brandt <sbrandt@cct.lsu.edu>
2026-07-15 13:55:35 -0500

    Update to-git.sh

--- bin/to-git.sh
+++ bin/to-git.sh
@@ -1,9 +1,21 @@
-URL=$(git config -l | grep 'remote.origin.url=' | cut -d= -f2)
-USER=$(echo $URL | cut -d/ -f4)
-REPO=$(echo $URL | cut -d/ -f5)
+if [ "$1" != "" ]; then
+    URL="$1"
+else
+    URL=$(git config -l | grep 'remote.origin.url=' | cut -d= -f2)
+fi
+if grep https > /dev/null <<< "${URL}"
+then
+    USER=$(echo $URL | cut -d/ -f4)
+    REPO=$(echo $URL | cut -d/ -f5)
+else
+    USER=$(echo $URL | cut -d: -f2 | cut -d/ -f1)
+    REPO=$(echo $URL | cut -d: -f2 | cut -d/ -f2)
+fi
 if grep bitbucket > /dev/null <<< "${URL}" 
 then
     echo "git remote set-url origin git@bitbucket.com:${USER}/${REPO}"
+    echo "git remote set-url origin https://bitbucket.com/${USER}/${REPO}"
 else
-    echo "git remote set-url origin git@github.com:${USER}/${REPO}.git"
+    echo "git remote set-url origin git@github.com:${USER}/${REPO}"
+    echo "git remote set-url origin https://github.com/${USER}/${REPO}"
 fi
